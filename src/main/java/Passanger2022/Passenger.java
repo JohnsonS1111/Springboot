2df7bfe8d3d526988b975fc18ae04a87cb613c70
Johnson <G00389668@atu.ie>
2022-11-05 19:18:58 +0000

    New branches added

--- src/main/java/Passanger2022/Passenger.java
+++ src/main/java/Passanger2022/Passenger.java
@@ -1,4 +1,5 @@
 package Passanger2022;
+import java.util.ArrayList;
 
 public class Passenger {
     private String Title;
@@ -26,6 +27,62 @@ public class Passenger {
 
         else{
             throw new IllegalArgumentException("Incorrect Title. Please enter 'Mr', 'Mrs', or 'Ms'.");
+        }
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        if(name.length() >= 3){
+            Name = name;
+        }
+
+        else{
+            throw new IllegalArgumentException("Incorrect Entry, Name must contain at least 3 characters");
+        }
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String id) {
+        if(id.length() >= 10){
+            this.ID = id;
+        }
+
+        else{
+            throw new IllegalArgumentException("Incorrect Entry, ID must contain at least 10 characters");
+        } 
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        if(phone.length() >= 7){
+            this.Phone = phone;
+        }
+
+        else{
+            throw new IllegalArgumentException("Incorrect Entry, Phone No. must contain at least 7 digits");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 16){
+            this.age = age;
+        }
+
+        else{
+            throw new IllegalArgumentException("Passenger is too young to fly");
         }
     }
 }
--- src/test/java/Passanger2022/PassengerTest.java
+++ src/test/java/Passanger2022/PassengerTest.java
@@ -3,6 +3,7 @@ package Passanger2022;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,6 +38,55 @@ class PassengerTest {
     void testTitleFail(){
         Exception exMessage = assertThrows(IllegalArgumentException.class, () -> { myPassenger.setTitle("xyz");});
         assertEquals("Incorrect Title. Please enter 'Mr', 'Mrs', or 'Ms'.", exMessage.getMessage());
+    }
+
+    @Test
+    void testName(){
+        myPassenger.setName("Zachary");
+        assertEquals("Zachary", myPassenger.getName());
+    }
+
+    @Test
+    void testNameFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> { myPassenger.setName("Ti");});
+        assertEquals("Incorrect Entry, Name must contain at least 3 characters", exMessage.getMessage());
+    }
+
+    @Test
+    void testID(){
+        myPassenger.setID("G00389668J");
+        assertEquals("G00389668J", myPassenger.getID());
+    }
+
+    @Test
+    void testIDFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> { myPassenger.setID("J00324");});
+        assertEquals("Incorrect Entry, ID must contain at least 10 characters", exMessage.getMessage());
+    }
+
+    @Test
+    void testPhone(){
+        myPassenger.setPhone("089222333");
+        assertEquals("089222333", myPassenger.getPhone());
+    }
+
+    @Test
+    void testPhoneFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> { myPassenger.setPhone("12345");});
+        assertEquals("Incorrect Entry, Phone No. must contain at least 7 digits", exMessage.getMessage());
+    }
+
+    @Test
+    void testAge(){
+        myPassenger.setAge(23);
+        assertEquals(23, myPassenger.getAge());
+    }
+
+    @Test
+    void testAgeFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> { myPassenger.setAge(12);});
+        assertEquals("Passenger is too young to fly", exMessage.getMessage());
+
     }
 
     @AfterEach
